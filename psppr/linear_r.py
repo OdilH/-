a526499b1a9d905f3dd1b1680d242304b17c8ae2
odil <l1m12345612@gmail.com>
2023-04-27 10:20:44 +0300

    Доделал линейную регрессию, корреляция переделана

--- psppr/linear_r.py
+++ psppr/linear_r.py
@@ -1,48 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Входные данные
-month = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-price = np.array([13, 20, 17, 15, 16, 12, 16, 14, 10, 11])
-sold = np.array([1000, 600, 500, 1200, 1000, 1500, 500, 1200, 1700, 2000])
+# Исходные данные
+x = np.array([13, 20, 17, 15, 16, 12, 16, 14, 10, 11])
+y = np.array([1000, 600, 500, 1200, 1000, 1500, 500, 1200, 1700, 2000])
 
-n = len(month)
-sum_price = np.sum(price)
-sum_sold = np.sum(sold)
-sum_price_squared = np.sum(price ** 2)
-sum_price_sold = np.sum(price * sold)
+# Вычисляем средние значения
+mean_x = np.mean(x)
+mean_y = np.mean(y)
 
-# Находим коэффициенты b0 и b1
-A = np.array([[n, sum_price], [sum_price, sum_price_squared]])
-B = np.array([sum_sold, sum_price_sold])
-b = np.linalg.solve(A, B)
+# Вычисляем коэффициенты b0 и b1
+numerator = sum((x - mean_x) * (y - mean_y))
+denominator = sum((x - mean_x) ** 2)
+b1 = numerator / denominator
+b0 = mean_y - b1 * mean_x
 
-b0, b1 = b
+# Находим предсказанные значения y
+y_pred = b0 + b1 * x
 
-# Предсказанные значения продаж
-predicted_sold = b0 + b1 * price
+# Вычисляем сумму квадратов ошибок и стандартную ошибку оценивания
+sq_errors = sum((y - y_pred) ** 2)
+mse = sq_errors / (len(x) - 2)
+rmse = np.sqrt(mse)
 
-# Расчет стандартной ошибки
-errors = predicted_sold - sold
-squared_errors = errors ** 2
-sum_squared_errors = np.sum(squared_errors)
-standard_error = np.sqrt(sum_squared_errors / (n - 2))
+# Вычисляем коэффициент корреляции
+# correlation_coeff = np.corrcoef(x, y)[0, 1]
 
-# Расчет коэффициента корреляции
-mean_price = np.mean(price)
-mean_sold = np.mean(sold)
-numerator = np.sum((price - mean_price) * (sold - mean_sold))
-denominator = np.sqrt(np.sum((price - mean_price) ** 2) * np.sum((sold - mean_sold) ** 2))
-correlation_coefficient = numerator / denominator
+# Вычисляем коэффициент корреляции без np.corrcoef
+numerator_correlation = np.sum((x - mean_x) * (y - mean_y))
+denominator_correlation = np.sqrt(np.sum((x - mean_x)**2) * np.sum((y - mean_y)**2))
+correlation_coeff = numerator_correlation / denominator_correlation
 
-print("Уравнение линейной регрессии: y = {:.2f} + {:.2f}x".format(b0, b1))
-print("Стандартная ошибка: {:.2f}".format(standard_error))
-print("Коэффициент корреляции: {:.4f}".format(correlation_coefficient))
 
-# График реальных и предсказанных значений продаж
-plt.scatter(price, sold, label='Реальные значения', color='blue')
-plt.plot(price, predicted_sold, label='Линия регрессии', color='red')
-plt.xlabel('Цена')
-plt.ylabel('Продажи')
+
+
+print("b0 =", b0)
+print("b1 =", b1)
+print("Уравнение регрессии: y = {:.2f} + {:.2f}x".format(b0, b1))
+print("Стандартная ошибка оценивания (RMSE):", rmse)
+print("Коэффициент корреляции:", correlation_coeff)
+
+# Визуализация данных и линии регрессии
+plt.axis([0, 25, 0, 2500])
+plt.scatter(x, y, label='Исходные данные', color='blue')
+plt.plot(x, y_pred, label='Линия регрессии', color='red')
+plt.xlabel('X')
+plt.ylabel('Y')
 plt.legend()
+plt.title('Линейная регрессия')
 plt.show()
